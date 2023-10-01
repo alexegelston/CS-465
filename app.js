b0f7f76488a4989d629059fa9ac39dfec8a2d72b
alexegelston <alexegelston@yahoo.com>
2023-09-30 21:58:09 -0800

    Create object schema with Mongoose, populate MongoDB

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const hbs = require('hbs');
+require('./app_server/models/db');
+
 
 var indexRouter = require('./app_server/routes/index');
 var usersRouter = require('./app_server/routes/users');
@@ -13,7 +15,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'app_server', 'views'));
   //register handlebars partials
-  hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
+hbs.registerPartials(path.join(__dirname, 'app_server', 'views/partials'))
 
 app.set('view engine', 'hbs');
 
--- app_server/controllers/tavel.js
+++ app_server/controllers/tavel.js
@@ -2,8 +2,7 @@ const fs = require('fs');
 const trips = JSON.parse(fs.readFileSync('./data/trips.json', 'utf8'));
 
 const travel = (req, res) => {
-    pageTitle = process.env.npm_package_description + ' - Travel';
-resizeBy.render('travel', {title: pageTitle, trips});
+    res.render('travel', {title: "Travlr Getaways - Travel", trips});
 };
 
 module.exports = {
